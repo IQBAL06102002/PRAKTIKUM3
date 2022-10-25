844cff2aabc93cd10bd28886c11090fe3467a928
M Iqbal Fadillah <miqbalfadillah6@gmail.com>
2022-10-26 05:51:41 +0700

    menambahkan file baru

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # PRAKTIKUM3
+
+# Nama : Muhammad Iqbal Fadillah
+
+# Kelas : TI.22 B.2
+
+# NIM : 312210586
+
+# Mata Kuliah = Bahasa Pemrograman
+
+# SOAL
+![Gambar1](1.png)
+# MASUKKAN NILAI JARI-JARI DAN RUMUS
+![Gambar2](2.png)
+# HITUNG LUAS LINGKARAN
+![Gambar3](3.png)
+# HITUNG KELILING LINGKARAN
+![Gambar4](4.png)
---
+++ main.py
@@ -0,0 +1,15 @@
+print("Diketahui jari-jari sebuah lingkaran 14 cm. Hitung Luas dan keliling lingkaran !")
+
+print("Diketahui:")
+r=14
+π=3.14 
+print("r=", r)
+print("π=", π)
+
+print("====LUAS LINGKARAN====")
+luas_lingkaran = π*r*r
+print("Hasil Luas Lingkaran=", luas_lingkaran)
+
+print("====KELILING LINGKARAN====")
+keliling_lingkaran= 2*π*r
+print("Hasil Keliling Lingkaran=", keliling_lingkaran)
